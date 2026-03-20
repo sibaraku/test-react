6c1e0bbc497114483ac9da3048bab66c42032ae2
sibaraku <189846437+sibaraku@users.noreply.github.com>
2026-03-20 12:32:46 +0200

    Ülesanne 5 - Valuuta andmete kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,14 @@
 const MealItem = (props) => {
+    const price = new Intl.NumberFormat("de-DE", {style: "currency",
+    currency: "EUR",}).format(props.meal.price);
+
     return (
         <li className="meal-item">
             <article className="meal-item article">
                 <img className="meal-item img" src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3 className="meal-item h3">{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">{price}</p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                     <p className="meal-item-actions">
