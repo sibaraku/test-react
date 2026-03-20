fc2bfc48233c18b6c541af298f6ecd8fed8497f1
sibaraku <189846437+sibaraku@users.noreply.github.com>
2026-03-20 10:52:30 +0200

    Clone repo + Ülesanne 1 - Header and Meals komponentide lisamine rakendusele

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+
 const App = () => {
- return (
+  return (
     <>
-     <h1>Food Order App</h1>
+      <Header />
+      <Meals />
     </>
   );
-}
+};
 
 export default App;
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -2,7 +2,7 @@ const Meals = () => {
     return (
         <ul id="meals">
             { 
-                // list of meals
+                <h2> list of meals</h2>
             }
         </ul>
     )
