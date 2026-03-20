06edbd2d621ca41c262bb8a784b50c248fc9abe2
sibaraku <189846437+sibaraku@users.noreply.github.com>
2026-03-20 11:21:30 +0200

    Ülesanne 3 - andmete pärimine Meals.js komponendis

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,23 @@
+import React, { useState, useEffect } from "react";
+
 const Meals = () => {
+    const [data, setData] = useState([]);
+    const url = "http://localhost:3001/meals";
+
+    const fetchData = () => {
+        return fetch(url)
+        .then((res) => res.json())
+        .then((d) => setData(d));
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    console.log(data);
+
     return (
         <ul id="meals">
             { 
-                <h2> list of meals</h2>
+                // list of meals
             }
         </ul>
     )
